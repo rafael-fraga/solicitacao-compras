be5509d2e4dbbd2fcfbc7de38b0f712ac7b503f6
Rafael Fraga <fragagreco@gmail.com>
2022-05-12 21:26:26 -0300

    12/05/2022 quinta: rota API e json

---
+++ api/produtos-api.py
@@ -0,0 +1,20 @@
+from flask import *
+import json, time, os
+
+app = Flask(__name__)
+
+data_dir = os.path.dirname(os.getcwd()) + '\\data\\'
+
+@app.route('/', methods=['GET'])
+def index():
+
+    f = open(data_dir + 'produtos.json', 'r')
+    produtos_json = f.read()
+
+    return json.dumps(produtos_json)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
---
+++ api/test.py
@@ -0,0 +1,5 @@
+import requests
+
+retorno = requests.get(url='http://127.0.0.1:5000/').json()
+
+print(retorno)
