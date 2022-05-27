e9de34c9a5c187b1f03d87377ec8ecd4141dbb36
Rafael Fraga <fragagreco@gmail.com>
2022-05-27 17:19:47 -0300

    modo debug, todos os produtos estão sendo retornados

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ from flask import *
 import os
 import science.produtos
 
+produtos_f = json.dumps(science.produtos.post_rota_produtos())
+
 app = Flask(__name__)
 
 cdir = os.getcwd() + '\\data\\'
@@ -12,29 +14,26 @@ def index():
 
 
 # INICIALIZAÇÃO
-
-# estoque (grid)
+# produtos
 @app.route('/produtos', methods=['GET'])
 def produtos():
-    return json.dumps(science.produtos.post_rota_produtos())
+    return produtos_f
 
 
 # FINALIZAÇÃOs
-
 # pedido (output do front end)
 @app.route('/pedido', methods=['POST'])
 def pedido():
-    
     science.pedidos.analise(json.loads(request.get_data()))
     return 'Análise concluída.'
 
 
 ## VARIÁVEIS DA ANÁLISE
-
 # prazos
 @app.route('/prazos', methods=['GET'])
 def prazos():
     return open(cdir + 'prazos.json', 'r').read()
+
 
 @app.route('/relatorio', methods=['POST'])
 def relatorio():
--- science/produtos.py
+++ science/produtos.py
@@ -5,7 +5,10 @@ from time import sleep
 
 def post_rota_produtos():
 
-    # ## Apurar lista generalizada de produtos
+    print('Importando produtos.')
+
+    ### removido para a versão debug ###
+    '''    # ## Apurar lista generalizada de produtos
     produtos = list()
     data = {'formato': 'json',
             'token': '2b6fc7102240cedcc9166c43921ea73eea82b876',
@@ -18,11 +21,15 @@ def post_rota_produtos():
             produtos += [x['produto'] for x in session['retorno']['produtos']]
         except:
             break
-    produtos = pd.DataFrame(produtos[:50])
+    produtos = pd.DataFrame(produtos[:50])'''
+
+
+    # variável temporária (remover antes do deploy)
+    ids = ['623449734', '623449795', '623449782', '778208483', '646749750', '729652533', '638403161', '623449824', '623449837', '661843960', '649329262', '733149637', '659216777', '623449886', '623449902', '692081848', '623449926', '706381781', '623449932', '623449946', '794828451', '773934587', '770209199', '778208038', '791977192', '663063892', '623449970', '744114547', '767806291', '799895288', '664322273', '638432052', '623449997', '789712012', '623450013', '649329268', '785293008', '623450189', '623450199', '800868965', '623450046', '623450182', '623450205', '623450210', '623450220', '682500784', '623450215', '804981063', '794827835', '653960156', '634824164', '623450225', '623450236', '628882233', '804677691', '786903341', '638379143', '623450327', '766299129', '623450340', '630551037', '623450354', '737002822', '774269426', '623450374', '733415440', '637005194', '795400098', '777807737', '626302922', '723855204', '623450496', '794829708', '801868011', '768983512', '623450527', '797788298', '778208806', '623450540', '643787433', '769851535', '623450553', '777808809', '623450568', '687343884', '687345363', '623450585', '687338443', '647280665', '623450611', '623450624', '623450740', '623450753', '640758281', '623450765', '626670053', '692082492', '623585662', '638867885', '640758272', '623450791', '647280670', '769175229']
 
     # Apurar databaset completo de produtos
     produtos_completos = list()
-    for id in produtos['id']:
+    for id in ids:                                                   ## será removido antes do deploy
         data = {
             'formato': 'json',
             'token': '2b6fc7102240cedcc9166c43921ea73eea82b876',
@@ -31,8 +38,9 @@ def post_rota_produtos():
         try:
             produtos_completos += [requests.post(url='https://api.tiny.com.br/api2/produto.obter.php', data=data).json()['retorno']['produto']]
         except:
-            print('limite de requisições')
+            print('limite de requisições atingido.')
             sleep(60)
+            print('Continuando.')
             produtos_completos += [requests.post(url='https://api.tiny.com.br/api2/produto.obter.php', data=data).json()['retorno']['produto']]
     produtos_completos = pd.DataFrame(produtos_completos)
 
@@ -42,10 +50,10 @@ def post_rota_produtos():
 
     # Limpeza de dados
     produtos_formatados = produtos_filtrados.drop(['preco', 'marca', 'preco_promocional', 'ncm', 'origem', 'gtin', 'gtin_embalagem', 'localizacao', 'peso_liquido', 'peso_bruto', 'estoque_maximo', 'id_fornecedor', 'codigo_fornecedor', 'codigo_pelo_fornecedor', 'unidade_por_caixa', 'preco_custo', 'preco_custo_medio', 'situacao', 'tipo', 'classe_ipi', 'valor_ipi_fixo', 'cod_lista_servicos', 'descricao_complementar', 'garantia', 'cest', 'obs', 'tipoVariacao', 'variacoes', 'idProdutoPai', 'sob_encomenda', 'dias_preparacao', 'tipoEmbalagem', 'alturaEmbalagem', 'comprimentoEmbalagem', 'larguraEmbalagem', 'diametroEmbalagem', 'categoria', 'anexos', 'imagens_externas', 'classe_produto', 'seo_title', 'seo_keywords', 'link_video', 'seo_description'], axis=1)
-
-    # unidade
     produtos_formatados['unidade'] = produtos_formatados['unidade'].str.casefold()
     produtos_formatados['unidade'] = ['un' if x != 'kg' else 'kg' for x in produtos_formatados['unidade']]
+
+    print('Importando estoque individual')
 
     # POST /produtos
     produtos_estoque = produtos_formatados
@@ -59,12 +67,12 @@ def post_rota_produtos():
         try:
             estoque += [requests.post(url='https://api.tiny.com.br/api2/produto.obter.estoque.php', data=data).json()['retorno']['produto']['saldo']]
         except:
-            print('limite de requisições')
+            print('limite de requisições atingido.')
             sleep(60)
+            print('Continuando.')
             estoque += [requests.post(url='https://api.tiny.com.br/api2/produto.obter.estoque.php', data=data).json()['retorno']['produto']]
     produtos_estoque['estoque'] = estoque
-    produtos_estoque.drop(['codigo', 'unidade', 'estoque_minimo', 'slug'], axis=1, inplace=True)
-
+    
     # POST /produtos
     return produtos_estoque.to_dict(orient='records')
 
