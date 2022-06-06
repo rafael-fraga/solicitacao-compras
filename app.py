acf328f5dbc7ef4463d40b11960e4c555b318dfd
Rafael Fraga <fragagreco@gmail.com>
2022-06-05 23:03:19 -0300

    sabia que não ia dar certo

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
 @app.route('/estoque', methods=['POST'])
 def produtos():
     file = request.files['0']
-    file.save('.data/estoque.xlsx')
+    file.save('./data/estoque.xlsx')
     response = jsonify({'status': 'success', 'mensagem': 'Erro de autênticação na linha x.', 'retorno': science.produtos.rota_estoque('.data/estoque.xlsx')})
     # response = jsonify({'status': 'success', 'mensagem': 'coluna 3 errada', 'retorno': 'json'})
     # response = jsonify({'status': 'error', 'mensagem': 'erro em tudo'})
